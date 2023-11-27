d02b4d9a5b26bda936e5785e181bd93f257e515e
Bo Tran <55688614+brass-pixel@users.noreply.github.com>
2023-11-27 12:55:55 +1100

    Finished project

--- script.js
+++ script.js
@@ -15,35 +15,36 @@ const endsWithWholeNumberRegex = /(?<!\.\d*)\d+$/;
 const endsWithOperatorRegex = /[+\-×÷]\s$/;
 const endsWithDecimalRegex = /\.\s*$/;
 
+// Maths Functions
 function add(a, b) {
 	let roundedNumber = parseFloat(a + b);
-  return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(3);
+	return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(2);
 }
 
 function subtract(a, b) {
 	let roundedNumber = parseFloat(a - b);
-  return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(3);
+	return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(2);
 }
 
 function multiply(a, b) {
 	let roundedNumber = parseFloat(a * b);
-  return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(3);
+	return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(2);
 }
 
 function divide(a, b) {
 	if (b == 0) {
-		return "Divide by zero error";
+		alert("Can't divide by Zero!");
+		return "0";
 	}
 	let roundedNumber = parseFloat(a / b);
-  return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(3);
+	return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(2);
 }
 
+// Evaluate equation
 function operate(num1, num2, operator) {
 	switch(operator) {
 		case "+":
 			calcInput.value = add(num1, num2);
-
-  //return Number.isInteger(roundedNumber) ? roundedNumber.toFixed(0) : roundedNumber.toFixed(3);
 			break;
 		case "-":
 			calcInput.value = subtract(num1, num2);
@@ -80,13 +81,25 @@ function equationCheck() {
 		let num1 = parseFloat(equationMatch[1])
 		let equation_operator = equationMatch[2]
 		let num2 = parseFloat(equationMatch[3])
-		console.log(equation_operator);
 		operate(num1, num2, equation_operator);
+		return true;
 	}
 
 }
 
+// Operator Function
+function handleOperator(operator) {
+  let equation = calcInput.value;
+  if (equation.match(numberPattern) || equationCheck()) {
+    appendOperator(operator);
+  }
+}
 
+// Back Function
+function backspaceDelete() {
+	let equation = calcInput.value.trim();
+	calcInput.value = equation.substring(0,(equation.length - 1)).trim();
+}
 /* Decimal Button */
 decimalButton.addEventListener('click', function() {
 	decimalCheck();
@@ -104,8 +117,7 @@ clearButton.addEventListener('click', function() {
 
 /* Back Function */
 deleteButton.addEventListener('click', function() {
-	let equation = calcInput.value.trim();
-	calcInput.value = equation.substring(0,(equation.length - 1)).trim();
+	backspaceDelete();
 });
 
 /* Input Numbers */
@@ -118,15 +130,49 @@ numberButtons.forEach(function(button) {
 
 
 /* Input Operators and Validation */
-
 operatorButtons.forEach(function(button) {
-	button.addEventListener('click', function(event) {
-		let equation = calcInput.value;
-		let operator = event.target.textContent;
-		// One Number only or Full Equation Check
-		if (equation.match(numberPattern) || equationCheck()) {	
-			appendOperator(operator)		}
-		
-		//(!(equation.match(endsWithDecimalRegex))) || (!(equation.match(endsWithOperatorRegex)))
+  button.addEventListener('click', function(event) {
+    let operator = event.target.textContent;
+    handleOperator(operator);
 	});
+});
+
+// Keyboard Mapping
+document.addEventListener('keydown', function(event) {
+	const key = event.key;
+
+	const numericKeys = /^[0-9]$/;
+	const operatorKeys = /^[+\-*\/]$/;
+	console.log(key);
+	// Number Input
+	if (key.match(numericKeys)) {
+		calcInput.value = calcInput.value + key;
+	}
+	// Operator Input
+	if (key.match(operatorKeys)) {
+		let operator = key;
+		switch(key) {
+			case "*":
+				operator = "×";	
+				break;
+			case "/":
+				operator = "÷";	
+				break;
+		}
+		handleOperator(operator);
+	}
+	switch (key) {
+		case '.':
+			decimalCheck();
+			break;
+		case 'Enter':
+			equationCheck();
+			break;
+		case 'Backspace':
+			backspaceDelete();
+			break;
+			// Add additional cases for other keys if needed
+		default:
+			break;
+	}
 });
